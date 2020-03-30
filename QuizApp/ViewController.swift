de709d9c948439a0769cffe70c1380ce4d063a88
Akhil George <akhilgeorge96@gmail.com>
2020-03-30 22:59:06 +1100

    Initial Commit

---
+++ QuizApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QuizApp
+//
+//  Created by Akhil George on 30/3/20.
+//  Copyright © 2020 Akhil George. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
